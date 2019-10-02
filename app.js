7ed8d20a2cb3d1a5b2bb9621867cadd97d851fe5
drmeloy <drmeloy@gmail.com>
2019-10-02 12:11:55 -0600

    added remaining content, styled page, wrote quiz js, wrote test js

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
+import { isCorrect } from './src/is-correct.js';
+
 const quizButton = document.getElementById("quiz-button");
 
 quizButton.onclick = () => {
-    alert('You ready to take the test?!')
+    const startTest = confirm('You ready to take the test?!');
+    console.log(startTest);
+
+    const question1 = prompt('Has Dan lived and worked on four continents?');
+    const answer1 = question1.toLowerCase().trim();
+    console.log(answer1);
+    if (isCorrect(answer1) === true){
+        alert('That\'s right! Dan has lived and worked in Thailand, New Zealand, Ecuador, and the USA!')
+    } else {
+        alert('Sorry! Dan has lived and worked on four continents.')
+    };
+
+    const question2 = prompt('Was the brewery Dan opened named Badass Brewing?');
+    const answer2 = question2.toLowerCase().trim();
+    console.log(answer2);
+    if (isCorrect(answer2) === true){
+        alert('Wrong! While that\'s a great name, Dan\'s brewery is named Bandido Brewing.')
+    } else {
+        alert('You got it! Dan\'s brewery is named Bandido Brewing.')
+    };
+
+    const question3 = prompt('Does Dan play the saxophone?');
+    const answer3 = question3.toLowerCase().trim();
+    console.log(answer3);
+    if (isCorrect(answer3) === true){
+        alert('Nooooo! He wishes! Dan mostly plays guitar though.')
+    } else {
+        alert('You\'re so smart! Dan has always wanted to play the sax though.')
+    };
+
+    const question4 = prompt('Does Dan have a twin sister?')
+    const answer4 = question4.toLowerCase().trim();
+    console.log(answer4);
+    if (isCorrect(answer4) === true){
+        alert('Nothing gets by you! Dan has a twin sister named Lindsay.')
+    } else {
+        alert('C\'mon! Were you even paying attention?!')
+    };
 }
---
+++ test/is-correct.test.js
@@ -0,0 +1,14 @@
+import { isCorrect } from "../src/is-correct.js";
+const test = QUnit.test;
+
+test('isCorrect should return true if user says yes', function(assert) {
+    const userAnswer = 'yes';
+    const userAnswerIsCorrect = isCorrect(userAnswer);
+    assert.equal(userAnswerIsCorrect, true);
+});
+
+test('isCorrect should return false if user says no', function(assert) {
+    const userAnswer = 'no';
+    const userAnswerIsCorrect = isCorrect(userAnswer);
+    assert.equal(userAnswerIsCorrect, false);
+});

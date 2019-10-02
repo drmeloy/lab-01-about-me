168945db65f91d21dce1b155acdcfe7b75bb7411
drmeloy <drmeloy@gmail.com>
2019-10-02 15:25:23 -0600

    added quiz results functionality

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const quizButton = document.getElementById('quiz-button');
 
 quizButton.onclick = () => {
     confirm('You ready to take the test?!');
+    let counter = 0;
 
     const question1 = prompt('Has Dan lived and worked on four continents?');
     const answer1 = question1.toLowerCase().trim();
     if (isCorrect(answer1) === true){
         alert('That\'s right! Dan has lived and worked in Thailand, New Zealand, Ecuador, and the USA!');
+        counter++;
     } else {
         alert('Sorry! Dan has lived and worked on four continents.');
     }
@@ -19,6 +21,7 @@ quizButton.onclick = () => {
         alert('Wrong! While that\'s a great name, Dan\'s brewery is named Bandido Brewing.');
     } else {
         alert('You got it! Dan\'s brewery is named Bandido Brewing.');
+        counter++;
     }
 
     const question3 = prompt('Does Dan play the saxophone?');
@@ -27,13 +30,23 @@ quizButton.onclick = () => {
         alert('Nooooo! He wishes! Dan mostly plays guitar though.');
     } else {
         alert('You\'re so smart! Dan has always wanted to play the sax though.');
+        counter++;
     }
 
     const question4 = prompt('Does Dan have a twin sister?');
     const answer4 = question4.toLowerCase().trim();
     if (isCorrect(answer4) === true){
         alert('Nothing gets by you! Dan has a twin sister named Lindsay.');
+        counter++;
     } else {
         alert('C\'mon! Were you even paying attention?!');
     }
+
+    const percent = (counter / 4) * 100;
+
+    confirm('Wow! You got ' + counter + ' out of 4 questions right! That\'s ' + percent + '%!');
+   
+    const quizResults = document.getElementById('quiz-results');
+    quizResults.textContent = 'You got a ' + percent + '% on the quiz about Dan! Click the button again if you want to retake the quiz!';
+    quizResults.classList.remove('hidden');
 };
